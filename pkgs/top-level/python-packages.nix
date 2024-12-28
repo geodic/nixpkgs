26d6602bbeb0d833b30e5b84425b21bfd81b8385
Otto Sabart <seberm@seberm.com>
2024-12-28 21:00:00 +0100

    python3Packages.yoto-api: init at 1.24.2

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -18315,6 +18315,8 @@ self: super: with self; {
     python3 = python;
   });
 
+  yoto-api = callPackage ../development/python-modules/yoto-api { };
+
   youless-api = callPackage ../development/python-modules/youless-api { };
 
   youseedee = callPackage ../development/python-modules/youseedee { };
