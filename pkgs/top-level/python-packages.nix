bd9fdeeb1b89437cedc664dde8f9921196122d36
Fabian Affolter <mail@fabian-affolter.ch>
2025-01-02 22:17:50 +0100

    python312Packages.json-api-doc: init at 0.15.0
    JSON API parser returning a simple Python dictionary
    
    https://github.com/julien-duponchelle/json-api-doc

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6695,6 +6695,8 @@ self: super: with self; {
 
   jsonlines = callPackage ../development/python-modules/jsonlines { };
 
+  json-api-doc = callPackage ../development/python-modules/json-api-doc { };
+
   json-logging = callPackage ../development/python-modules/json-logging { };
 
   jsonmerge = callPackage ../development/python-modules/jsonmerge { };
